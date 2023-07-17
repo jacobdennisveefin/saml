cac82234a62eb396307f8e19286e9e15db3f8a4f
Jacob Dennis P <116080050+jacobdennisveefin@users.noreply.github.com>
2023-07-17 16:48:10 +0530

    changes

--- app/Http/Controllers/SamlController.php
+++ app/Http/Controllers/SamlController.php
@@ -5,15 +5,28 @@ use Aacotroneo\Saml2\Http\Controllers\Saml2Controller;
 use Illuminate\Http\Request;
 use Aacotroneo\Saml2\Saml2Auth;
 use Aacotroneo\Saml2\Saml2User;
+use DOMDocument;
 use XMLParser;
 
 class SamlController extends Controller
 {
     public function acs()
     {
-     //   dd(Saml2User);
-     $xml = base64_decode(request()->get('SAMLResponse'));
-     
-    $xml = XMLParser::load($xml);
+        $dom = new \DOMDocument();
+        $dom->loadXML(base64_decode(request()->get('SAMLResponse')));
+        $doc = $dom->documentElement;
+        $xpath = new \DOMXpath($dom);
+        $xpath->registerNamespace('samlp', 'urn:oasis:names:tc:SAML:2.0:protocol');
+        $xpath->registerNamespace('saml', 'urn:oasis:names:tc:SAML:2.0:assertion');
+        $globalAttrArr = [];
+        foreach ($xpath->query('/samlp:Response/saml:Assertion/saml:AttributeStatement/saml:Attribute', $doc) as $attr) {
+            $attrArr = [];           
+            foreach ($xpath->query('saml:AttributeValue', $attr) as $value) {
+                $attrArr[$attr->getAttribute('Name')] = $value->textContent;
+            }
+            $globalAttrArr[$attr] = $attrArr;
+        }
+        dd($globalAttrArr);
+        
     }
 }
