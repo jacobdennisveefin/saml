e7c12ec3a583de4bd406596cd86a5011a4de2d6a
Jacob Dennis P <116080050+jacobdennisveefin@users.noreply.github.com>
2023-07-17 11:26:00 +0530

    changes

---
+++ app/Http/Controllers/SamlController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use Aacotroneo\Saml2\Http\Controllers\Saml2Controller;
+use Illuminate\Http\Request;
+
+class SamlController extends Saml2Controller
+{
+    public function login()
+    {
+        
+    }
+}
