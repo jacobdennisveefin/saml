90b974510fa71a0ce4521d6cb7596331c368419d
Jacob Dennis P <116080050+jacobdennisveefin@users.noreply.github.com>
2023-07-17 12:37:20 +0530

    Changes

--- config/app.php
+++ config/app.php
@@ -212,6 +212,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Saml2' => 'Aacotroneo\Saml2\Facades\Saml2Auth',
     ])->toArray(),
 
 ];
