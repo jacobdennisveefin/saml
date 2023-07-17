40be84c607b55a9f159009e0738a2e3a67c51720
Jacob Dennis P <116080050+jacobdennisveefin@users.noreply.github.com>
2023-07-17 12:38:57 +0530

    Changes

--- app/Http/Controllers/SamlController.php
+++ app/Http/Controllers/SamlController.php
@@ -5,7 +5,7 @@ use Aacotroneo\Saml2\Http\Controllers\Saml2Controller;
 use Illuminate\Http\Request;
 use Aacotroneo\Saml2\Saml2Auth;
 
-class SamlController extends Saml2Controller
+class SamlController extends Controller
 {
     public function login()
     {
