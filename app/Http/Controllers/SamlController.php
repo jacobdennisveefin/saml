d18f58f17975e8de9b826b982f0441a114254db9
Jacob Dennis P <116080050+jacobdennisveefin@users.noreply.github.com>
2023-07-17 16:05:55 +0530

    changes

--- app/Http/Controllers/SamlController.php
+++ app/Http/Controllers/SamlController.php
@@ -5,13 +5,15 @@ use Aacotroneo\Saml2\Http\Controllers\Saml2Controller;
 use Illuminate\Http\Request;
 use Aacotroneo\Saml2\Saml2Auth;
 use Aacotroneo\Saml2\Saml2User;
-use Orchestra\Parser\Xml\Facade as XmlParser;
+use XMLParser;
 
 class SamlController extends Controller
 {
     public function acs()
     {
      //   dd(Saml2User);
-     dd(base64_decode(request()->get('SAMLResponse')));
+     $xml = base64_decode(request()->get('SAMLResponse'));
+     
+    $xml = XMLParser::load($xml);
     }
 }
